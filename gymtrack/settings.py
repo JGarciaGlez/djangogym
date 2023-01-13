e735951d311413c3a0ab267ccf4a91c81339a24c
javier <elteliblejvi@gmail.com>
2023-01-13 16:46:28 +0100

    fix errors

--- gymtrack/settings.py
+++ gymtrack/settings.py
@@ -84,8 +84,8 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = 'static/'
- environments.
+STATIC_URL = '/static/'
+
 if not DEBUG:    
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
